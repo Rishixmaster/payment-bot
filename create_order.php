5579c576ede0b5bbf112b68f2c6c51bdd52171ca
Rishixmaster <144030853+Rishixmaster@users.noreply.github.com>
2026-05-08 16:36:31 +0530

    Update create_order.php

--- create_order.php
+++ create_order.php
@@ -1,5 +1,5 @@
 <?php
-// create_order.php
+// create_order.php - Debug Version
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -12,15 +12,21 @@ if (isset($_POST['action']) && $_POST['action'] === 'create_order') {
         "appNo"       => "KARMAXRISHI",
         "callbackUrl" => "https://mlpay-bot.onrender.com/callback.php",
         "name"        => "User",
-        "remark"      => "Telegram Bot Deposit",
+        "remark"      => "Telegram Deposit",
         "orderCode"   => "TG_" . time() . rand(10000,99999)
     ];
+
+    // === Debug Log ===
+    file_put_contents('sign_debug.log', date('H:i:s') . " - Params: " . json_encode($params) . "\n", FILE_APPEND);
 
     ksort($params);
     $query_string = http_build_query($params);
     $secret_key = "dtdLggJaWaltMXrtJVVs";
     $final_string = $query_string . "&key=" . $secret_key;
+    
     $signature = strtolower(md5($final_string));
+
+    file_put_contents('sign_debug.log', date('H:i:s') . " - Final String: " . $final_string . "\nSignature: " . $signature . "\n\n", FILE_APPEND);
 
     $params["sign"] = $signature;
 
@@ -34,6 +40,8 @@ if (isset($_POST['action']) && $_POST['action'] === 'create_order') {
     $response = curl_exec($ch);
     curl_close($ch);
 
+    file_put_contents('sign_debug.log', date('H:i:s') . " - ML Pay Response: " . $response . "\n\n", FILE_APPEND);
+
     if ($response) {
         $result = json_decode($response, true);
         if (isset($result['code']) && $result['code'] == 200) {
@@ -43,14 +51,13 @@ if (isset($_POST['action']) && $_POST['action'] === 'create_order') {
                 "orderCode" => $result['data']['orderCode']
             ]);
         } else {
-            echo json_encode(["error" => $result['msg'] ?? "API Error"]);
+            echo json_encode(["error" => $result['msg'] ?? "Signature Error"]);
         }
     } else {
-        echo json_encode(["error" => "No response from ML Pay"]);
+        echo json_encode(["error" => "No response"]);
     }
     exit;
 }
 
-// Browser test
-echo "create_order.php is working";
+echo "create_order.php loaded";
 ?>
